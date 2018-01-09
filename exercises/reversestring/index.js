eda7a9e99e25345ccec484af95bc84f1ae9886f8
Oscar Chavez <oscarchavezromero@gmail.com>
2018-01-09 14:31:31 -0800

    initial

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,31 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+    //Solution 1
+    //return str.split('').reverse().join('');
+
+    //Solution 2 #  Old fashioned
+    /*
+    let reversed = new Array();
+    for(var i = 0; i <= arr.length; i++){
+        let temp = arr[arr.length-i];
+        reversed[i] = temp;
+    }
+    
+    return reversed.join('');
+
+    */
+    /*
+    for(let char of arr){
+        reversed = char + reversed;
+    }
+    return reversed;
+    */
+
+    //Solution 3 # Newest ES6 
+    return str.split('').reduce((rev, char)=> char + rev, '');
+
+}
 
 module.exports = reverse;
