2371db565c862e2b766ca3dc404324c811aabf7b
oschvr <oscarchavezromero@gmail.com>
2018-01-10 09:13:22 -0600

    Reverse String && Palindrome completed

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -7,6 +7,20 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {}
+function palindrome(str) {
+
+    //Solution 1
+    //const reversed = str.split('').reverse().join('');
+    //return str === reversed;
+
+    //Solution 2
+    //const reversed = str.split('').reduce((rev, char) => char + rev, '');
+    //return str === reversed;
+
+    //Solution 3 | Using every()
+    return str.split('').every((char, i) => {
+        return char === str[str.length - i - 1];
+    });
+}
 
 module.exports = palindrome;
