79a1bef605a22dae4d8c216c8a642f2c72012520
oschvr <oscarchavezromero@gmail.com>
2018-02-09 12:14:12 -0600

    Chunk + fizzbuzz excercises done

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,29 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk(array, size) {
+  
+  const chunked = [];
+  let index = 0;
+
+  //newArr.push(array.splice(0, size));
+  /*
+  for(let element of array){
+    const last = chunked[chunked.length - 1];
+
+    if(!last || last.length === size){
+      chunked.push([element]);
+    } else {
+      last.push(element);
+    }
+  }*/
+  
+  while(index < array.length) {
+    chunked.push(array.slice(index, index + size));
+    index += size;
+  }
+
+  return chunked;
+}
 
 module.exports = chunk;
