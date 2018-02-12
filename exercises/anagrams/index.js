832e881df31fdaee4b9dc7108dcc9d6b81f7ef50
oschvr <oscarchavezromero@gmail.com>
2018-02-12 10:36:02 -0600

    Done anagrams

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,50 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function anagrams(stringA, stringB) {
+
+  /*
+  let strA = charMap(stringA);
+  let strB = charMap(stringB);
+
+  if(Object.keys(strA).length != Object.keys(strB).length) {
+    return false;
+  } 
+
+  for(let char in strA){
+    if(strA[char] !== strB[char]){
+      return false;
+    }
+    return true;
+  }
+  */
+
+  if(clean(stringA) == clean(stringB)){
+    return true;
+  } else {
+    return false;
+  }
+
+}
+
+function clean(str){
+  return str
+    .replace(/[^\w]/g, '')
+    .toLowerCase()
+    .split('')
+    .sort('')
+    .join('');
+}
+
+// Helper function to build a Character Map of {key, val}
+function charMap(str){
+  let charMap = {};
+
+  for (let char of str.replace(/[^\w]/g, '').toLowerCase()){
+    charMap[char] = charMap[char] + 1 || 1;
+  }
+
+  return charMap;
+}
 
 module.exports = anagrams;
