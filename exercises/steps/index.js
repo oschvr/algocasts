cc1107fd544d48ba6a3f64897e4de66ce4f57014
oschvr <oscarchavezromero@gmail.com>
2018-02-15 10:20:03 -0600

    Steps done

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,6 +17,18 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+function steps(n) {
+    for (let i = 0; i < n; i++) {
+        let stair = "";
+        for(let j = 0; j < n; j++){
+            if(j <= i){
+                stair+="#";
+            } else {
+                stair+=" ";
+            }
+        }
+        console.log(stair);
+    }
+}
 
 module.exports = steps;
